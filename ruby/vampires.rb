4ee658ac409dc17b9213a01d3e6430c476fdf670
Jonathan <hyunjonathan@gmail.com>
2016-11-18 10:08:33 -0600

    finished input

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+print "What is your name? "
+name= gets.chomp
+
+print "How old are you? "
+age= gets.chomp
+
+print "What year were you born? "
+year=gets.chomp
+
+print "Our company cafeteria serves garlic bread."
+print "Should we order some for you?(y/n) "
+garlic=gets.chomp
+
+print "Would you like to enroll in the company's health insurance? "
+insurance=gets.chomp
+
+
