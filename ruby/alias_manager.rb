5ffe15a9d844784bd2d0445c259efd7abc2c7b72
Jonathan <hyunjonathan@gmail.com>
2016-11-26 13:41:16 -0600

    release 0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,53 @@
+def letter_changer(word)
+	index=0
+	while index<word.length
+		case word[index]
+		when "a"
+			word[index]="e"
+		when "e"
+			word[index]="i"
+		when "i"
+			word[index]="o"
+		when "o"
+			word[index]="u"
+		when "u"
+			word[index]="a"
+		when " "
+			word[index]=word[index]
+		else
+			word[index]=word[index].next
+			if word[index].match(/[aeiou]/)
+				word[index]=word[index].next!
+			end
+		end
+		index+=1
+	end
+	return word
+end
+
+			
+def capitalize(word)
+	word=word.split(" ")
+	word.map! {|x| x.capitalize!}
+	word=word.join(" ")
+	return word
+end
+
+
+
+def alias_maker(name)
+	name.downcase!
+	name=name.split(" ")
+	name=name.reverse!
+	name=name.join(" ")
+	name=name.split("")
+	name=letter_changer(name)
+	name=name.join("")
+	return name
+end
+
+cover_name=alias_maker("Felicia Torres")
+cover_name=capitalize(cover_name)
+puts cover_name
+
+
