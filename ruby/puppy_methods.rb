a0533895dd60fc9030d8fc041d4582c9c3ddbdca
Jonathan <hyunjonathan@gmail.com>
2016-12-01 13:41:38 -0600

    initializing method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,40 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+  def dog_years(human_years)
+    dog_years = human_years / 15
+  end
+  def play_dead
+    puts "*Gets hit by CAR!*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
 end
+
+
+#DRIVER CODE
+pup = Puppy.new
+
+pup.fetch("big ball")
+
+pup.speak(5)
+
+pup.roll_over
+
+puts pup.dog_years(15)
+
+pup.play_dead
+
 
