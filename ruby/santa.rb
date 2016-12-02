423f9d58a8062ec10493faa76c51c2f25367c5de
Jonathan <hyunjonathan@gmail.com>
2016-12-02 13:15:17 -0600

    added santa class and instances

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+
+	def speak
+		puts"Ho, ho, ho! Haaaapy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+end
+
