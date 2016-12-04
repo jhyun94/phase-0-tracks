76a4ead51e1f9881dab9df5cfd0f6075431aaa75
Jonathan <hyunjonathan@gmail.com>
2016-12-03 19:26:45 -0600

    added mixin modules with 2 classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
@@ -10,3 +11,37 @@ end
 
 puts Shout.yell_angrily("STOPPP")
 puts Shout.yell_happily("HII")
+
+=end
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + "!!!" + " :("
+	end
+
+end
+
+class Dwarf
+	include Shout
+end
+
+class Baby
+	include Shout
+end
+
+Janna = Baby.new
+puts Janna.yell_happily("I love cookies")
+puts Janna.yell_angrily("I hate ice cream")
+
+Axel = Dwarf.new
+puts Axel.yell_happily("I love axes")
+puts Axel.yell_angrily("I hate giants")
+
+
+
+
+		
