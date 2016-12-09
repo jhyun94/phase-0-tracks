0466edfaffbcf4315c671bcf716a73b4d2a36a66
Jonathan <hyunjonathan@gmail.com>
2016-12-09 17:10:17 -0600

    created new horse object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,5 @@
 var colors = ["blue", "pink", "yellow","orange"];
-var names = ["Ed", "Tim"," Rose", "Sara"];
+var names = ["Ed", "Tim","Rose", "Sara"];
 
 colors.push("purple");
 names.push("Laura");
@@ -7,3 +7,13 @@ names.push("Laura");
 console.log(colors);
 console.log(names);
 
+var horses={};
+var list=[];
+if (colors.length == names.length) {
+	for(var i=0;i<colors.length;i++){
+		var key = names[i];
+		horses[key]=colors[i];
+	};
+};
+list.push(horses);
+console.log(list);
