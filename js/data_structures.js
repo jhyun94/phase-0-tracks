636bd5ae5077345007c52310f24d7c2d5954252e
Jonathan <hyunjonathan@gmail.com>
2016-12-09 16:09:11 -0600

    Added 2 arrays and pushed new elements

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "pink", "yellow","orange"];
+var names = ["Ed", "Tim"," Rose", "Sara"];
+
+colors.push("purple");
+names.push("Laura");
+
+console.log(colors);
+console.log(names);
+
