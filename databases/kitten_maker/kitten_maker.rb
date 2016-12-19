c03a8e2f4910ba556b26061a7fc887cff03a893a
Jonathan <hyunjonathan@gmail.com>
2016-12-19 11:36:45 -0600

    .

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -7,8 +7,9 @@ require 'sqlite3'
 require 'faker'
 
 # create SQLite3 database
+
 db = SQLite3::Database.new("kittens.db")
-db.results_as_hash = true
+#db.results_as_hash = true
 
 # learn about fancy string delimiters
 create_table_cmd = <<-SQL
@@ -22,8 +23,10 @@ SQL
 # create a kittens table (if it's not there already)
 db.execute(create_table_cmd)
 
+
 # add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+ db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+
 
 # add LOOOOTS of kittens!
 # so. many. kittens. 
@@ -32,13 +35,15 @@ def create_kitten(db, name, age)
   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
 end
 
-10000.times do
+10.times do
   create_kitten(db, Faker::Name.name, 0)
 end
 
 # explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
+=begin
+kittens = db.execute("SELECT * FROM kittens")
+kittens.each do |kitten|
+puts "#{kitten['name']} is #{kitten['age']}"
+end
 
+=end
